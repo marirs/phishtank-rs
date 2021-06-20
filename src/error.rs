a343965a3ffdbd45dce5e3fd9634186ae7df7d32
Sriram Govindan <marirs@gmail.com>
2021-06-20 18:26:09 +0530

    update

---
+++ src/error.rs
@@ -0,0 +1,36 @@
+use reqwest::StatusCode;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum PhishtankError {
+    #[error("The server has exceeded the bandwidth specified by the server administrator.")]
+    BandwidthExceeded,
+    #[error("Unknown error.")]
+    Unknown,
+    #[error("{0}")]
+    Io(#[from] std::io::Error),
+    #[error("{0}")]
+    Json(serde_json::Error),
+    #[error("{0}")]
+    Reqwest(#[from] reqwest::Error),
+}
+
+impl From<serde_json::Error> for PhishtankError {
+    fn from(err: serde_json::Error) -> PhishtankError {
+        use serde_json::error::Category;
+        match err.classify() {
+            Category::Io => PhishtankError::Io(err.into()),
+            Category::Syntax | Category::Data | Category::Eof => PhishtankError::Json(err),
+        }
+    }
+}
+
+/// Return the PhishtankError based on the http status code
+impl From<StatusCode> for PhishtankError {
+    fn from(s: StatusCode) -> PhishtankError {
+        match s.as_str() {
+            "509" => PhishtankError::BandwidthExceeded,
+            _ => PhishtankError::Unknown,
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+///
+mod download;
+mod utils;
+
+pub mod error;
+use error::PhishtankError;
+
+pub type PhishtankResult<T> = Result<T, PhishtankError>;
+
+static DEFAULT_USER_AGENT: &str = "rust-client/phishtank+https://github.com/marirs/phishtank-rs";
+
+#[derive(Clone)]
+pub struct PhishtankClient {
+    api_key: String,
+    data_endpoint: String,
+    check_endpoint: String,
+    user_agent: String,
+}
+
+impl PhishtankClient {
+    pub fn new(api_key: &str) -> Self {
+        //! Creates a new Phishtank API Client to access the Phishtank API endpoint
+        //!
+        //! ## Example usage
+        //! ```rust
+        //! use phishtank::PhishtankClient;
+        //!
+        //! let client = PhishtankClient::new("YOUR API KEY");
+        //! ```
+        PhishtankClient {
+            api_key: api_key.into(),
+            data_endpoint: "https://data.phishtank.com/data".into(),
+            check_endpoint: "https://checkurl.phishtank.com/checkurl".into(),
+            user_agent: DEFAULT_USER_AGENT.into(),
+        }
+    }
+
+    /// Sets a new user-agent that from the default
+    pub fn user_agent(mut self, user_agent: &str) -> PhishtankClient {
+        self.user_agent = user_agent.into();
+        self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::PhishtankClient;
+
+    #[test]
+    fn test_vtclient() {
+        let client = PhishtankClient::new("someapikey");
+        assert_eq!(client.api_key, "someapikey");
+        assert_eq!(client.data_endpoint, "https://data.phishtank.com/data");
+        assert_eq!(client.check_endpoint, "https://checkurl.phishtank.com/checkurl");
+    }
+}
